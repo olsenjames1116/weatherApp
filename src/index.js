099cebffb49145dd2d103370cfff2a823cd59cb5
James Olsen <olsenjames1116@gmail.com>
2023-01-29 15:32:38 -0700

    Validate search input
    An event listener was added to the submit button to validate input. This
    is a basic validation to ensure the user did not attempt to skip it or
    input anything other than letters.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import './style.css';
 
-const submitButton = document.querySelector('form>div>button');
+const form = document.querySelector('form');
+const searchBar = document.querySelector('input');
+const submitButton = document.querySelector('[type=submit]');
 
+function showSearchError() {
+    if (searchBar.validity.valueMissing) {
+        searchBar.setCustomValidity('Please enter a city');
+    } else if (searchBar.validity.patternMismatch) {
+        searchBar.setCustomValidity('Please only enter letters');
+    } else {
+        searchBar.setCustomValidity("");
+    }
+
+    searchBar.reportValidity();
+}
+
+function validateInput() {
+    let location;
+
+    if (form.checkValidity()) {
+        location = searchBar.value;
+        console.log(location);
+    } else {
+        showSearchError();
+    }
+}
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    validateInput();
+})
