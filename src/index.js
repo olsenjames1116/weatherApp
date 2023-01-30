56e1b5ad19789a215f00b9a31d35db0034e38087
James Olsen <olsenjames1116@gmail.com>
2023-01-29 17:31:46 -0700

    Move validation to validate module

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
 import './style.css';
+import { location, validateInput } from './validate';
 
-const form = document.querySelector('form');
-const searchBar = document.querySelector('input');
 const submitButton = document.querySelector('[type=submit]');
-let location;
 
-function clearInput() {
-    searchBar.value = '';
-}
+function displayWeather(data) {
 
-function showSearchError() {
-    if (searchBar.validity.valueMissing) {
-        searchBar.setCustomValidity('Please enter a city');
-    } else if (searchBar.validity.patternMismatch) {
-        searchBar.setCustomValidity('Please only enter a city or a city followed with a comma and either a state (US only) or a country code. Ex: Denver, Colorado');
-    } else {
-        searchBar.setCustomValidity("");
-    }
-
-    searchBar.reportValidity();
-}
-
-function validateInput() {
-    if (form.checkValidity()) {
-        location = searchBar.value;
-        clearInput();
-        return true;
-    } 
-
-    showSearchError();
-    return false;
 }
 
 async function getWeather() {
@@ -37,6 +12,7 @@ async function getWeather() {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=faefb21b364d236534cc9f8b0216f294`);
         const data = await response.json();
         console.log(data);
+        displayWeather(data);
     } catch(error) {
         alert(error);
     }
---
+++ src/validate.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+const searchBar = document.querySelector('input');
+// eslint-disable-next-line import/no-mutable-exports
+let location;
+
+function clearInput() {
+    searchBar.value = '';
+}
+
+function showSearchError() {
+    if (searchBar.validity.valueMissing) {
+        searchBar.setCustomValidity('Please enter a city');
+    } else if (searchBar.validity.patternMismatch) {
+        searchBar.setCustomValidity('Please only enter a city or a city followed with a comma and either a state (US only) or a country code. Ex: Denver, Colorado');
+    } else {
+        searchBar.setCustomValidity("");
+    }
+
+    searchBar.reportValidity();
+}
+
+function validateInput() {
+    if (form.checkValidity()) {
+        location = searchBar.value;
+        clearInput();
+        return true;
+    } 
+
+    showSearchError();
+    return false;
+}
+
+export { location, validateInput }
