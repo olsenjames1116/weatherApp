0b63ad0425046a242157d6184867158aa0253214
James Olsen <olsenjames1116@gmail.com>
2023-01-29 17:06:28 -0700

    Fix validation on search input
    The regex was updated on the search bar input to allow for a user to
    enter either a city or a city and state (US only) or city and country
    code.
    
    A return value was given to the validation function in the JS file. This
    return value is used to halt execution unless a valid input is entered.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function showSearchError() {
     if (searchBar.validity.valueMissing) {
         searchBar.setCustomValidity('Please enter a city');
     } else if (searchBar.validity.patternMismatch) {
-        searchBar.setCustomValidity('Please only enter letters');
+        searchBar.setCustomValidity('Please only enter a city or a city followed with a comma and either a state (US only) or a country code. Ex: Denver, Colorado');
     } else {
         searchBar.setCustomValidity("");
     }
@@ -25,12 +25,28 @@ function validateInput() {
     if (form.checkValidity()) {
         location = searchBar.value;
         clearInput();
-    } else {
-        showSearchError();
+        return true;
+    } 
+
+    showSearchError();
+    return false;
+}
+
+async function getWeather() {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=faefb21b364d236534cc9f8b0216f294`);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        alert(error);
     }
 }
 
 submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-    validateInput();
-})
+    const validInput = validateInput();
+
+    if (validInput) {
+        getWeather();
+    }
+});
